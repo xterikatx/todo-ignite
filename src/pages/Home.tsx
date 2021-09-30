66abe63aacdf2af77a79835054beeaf64e4e334a
Erika Lopes <201810099@acad.unijuazeiro.edu.br>
2021-09-30 09:04:30 -0300

    Add marker task

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,22 +9,25 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
-    const data = {
+    const newTask = {
       id: Number(new Date().getTime()),
       title: newTaskTitle,
-      done: false
-    }
-    setTasks(oldState => [...oldState, data]);
-
+      done: false,
+    };
+    setTasks((oldState) => [...oldState, newTask]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updatedTasks = [...tasks];
+
+    const findTask = updatedTasks.find((task) => task.id === id);
+    if (!findTask) return;
+    findTask.done = !findTask.done;
+    setTasks(updatedTasks)
   }
 
   function handleRemoveTask(id: number) {
-    setTasks(oldState => oldState.filter(task => task.id !== id));
+    setTasks((oldState) => oldState.filter((task) => task.id !== id));
   }
 
   return (
